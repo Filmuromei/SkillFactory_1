210bc8960f6f7fdd89f536ea5603ff797d46bc27
Filmuromei <138862583+Filmuromei@users.noreply.github.com>
2023-07-12 10:45:30 +0300

    Add files via upload

--- Krestic_Nolic.py
+++ Krestic_Nolic.py
@@ -3,15 +3,15 @@ print("Крестики нолики версия 1.0")
 board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 #Доска
 def draw_board(board):
-    print("*"* 6)
+    print("*" * 6)
     for i in range(3):
-        print(board[i + 0 *3], board[i + 1 *3],board[i + 2 *3])
+        print(board[i + 0 * 3], board[i + 1 * 3], board[i + 2 * 3])
         print("*" * 6)
 # Ввод данных
 def input_player(player):
     while True:
         hod_1 = input("Введите число" + player + ":")
-        if not (hod_1 in "123456789"):
+        if len(hod_1) != 1:
             print("Введите пожалуйста число!")
             continue
         hod_1 = int(hod_1)
@@ -20,6 +20,15 @@ def input_player(player):
             continue
         board[hod_1 - 1] = player
         break
+def win():
+    winner = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7), (2, 5, 8), (3, 6, 9), (1, 5, 9), (7, 5, 3)]
+    for i in winner:
+        if board[i[0]-1] == board[i[1]-1] == board[i[2]-1]:
+            return board[i[1] - 1]
+    else:
+        return False
+
+
 # Функция чтоб все работало на конец то !!!!
 def main(board):
     count = 0
@@ -29,7 +38,20 @@ def main(board):
             input_player("X")
         else:
             input_player("O")
-        count+=1
+        if count > 3:
+            wins = win()
+            if wins:
+                draw_board(board)
+                print(wins , "Победа ")
+                break
+        count += 1
+        if count > 8:
+            wins = win()
+            if wins:
+                draw_board(board)
+                print(wins, "Ничья ")
+                break
+
 main(board)
 
 
